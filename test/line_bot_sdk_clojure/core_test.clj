330dbcd3ba743edf5181bbc65c25f4d315532c07
xorphitus <xorphitus@gmail.com>
2016-12-16 00:28:16 +0900

    initial commit

---
+++ test/line_bot_sdk_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns line-bot-sdk-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [line-bot-sdk-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
